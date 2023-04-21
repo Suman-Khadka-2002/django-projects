57c8d46d8fa3bc3ab0b860331566a71a741e182d
Suman Khadka <dnatsu022@gmail.com>
2023-04-21 21:38:01 +0545

    add two number and display

--- myFirstProject/calc/views.py
+++ myFirstProject/calc/views.py
@@ -5,3 +5,10 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, 'home.html', {'name':'Suman'})
+
+def add(request):
+
+    value1 = int(request.GET['num1'])
+    value2 = int(request.GET['num2'])
+    res = value1 + value2
+    return render(request, 'result.html', {'result':res})
