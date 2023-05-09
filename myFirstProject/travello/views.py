be9db56d7940c2fc9b078f1d8510d5f91ade5844
Suman Khadka <dnatsu022@gmail.com>
2023-05-09 21:13:09 +0545

    passing data as dictonary format to the index.html

--- myFirstProject/travello/views.py
+++ myFirstProject/travello/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-
+from .models import Destination
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+
+    dest1 = Destination()
+    dest1.name = "Kathmandu"
+    dest1.desc = "Capital city"
+    dest1.price = 700
+
+    return render(request, "index.html", {'dest1': dest1})
