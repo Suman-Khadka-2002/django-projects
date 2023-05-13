6241118432d226800bf2df80c7d423b012e7b38c
Suman Khadka <dnatsu022@gmail.com>
2023-05-13 19:56:37 +0545

    fetching data dynamically through database

--- myFirstProject/travello/views.py
+++ myFirstProject/travello/views.py
@@ -4,27 +4,6 @@ from .models import Destination
 
 def index(request):
 
-    dest1 = Destination()
-    dest1.img = 'destination_1.jpg'
-    dest1.name = "Kathmandu"
-    dest1.desc = "Capital city"
-    dest1.price = 700
-    dest1.offer = False
-
-    dest2 = Destination()
-    dest2.img = 'destination_2.jpg'
-    dest2.name = "Pokhara"
-    dest2.desc = "City of lakes and ponds"
-    dest2.price = 1500
-    dest2.offer = True
-
-    dest3 = Destination()
-    dest3.img = 'destination_3.jpg'
-    dest3.name = "Illam"
-    dest3.desc = "City famous for the tea leaves"
-    dest3.price = 1000
-    dest3.offer = False
-
-    dests = [dest1, dest2, dest3]
+    dests = Destination.objects.all()  # fetching data
 
     return render(request, "index.html", {'dests': dests})
