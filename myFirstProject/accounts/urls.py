24d6bac41260d71844dd21e1e13d7ac7d81d4221
Suman Khadka <dnatsu022@gmail.com>
2023-05-16 16:01:20 +0545

    path to the login in views

--- myFirstProject/accounts/urls.py
+++ myFirstProject/accounts/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register', views.register, name='register') # gives path to the register.html
+    path('register', views.register, name='register'), # gives path to the register.html
+    path('login', views.login, name= 'login'),
 ]
