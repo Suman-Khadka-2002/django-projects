37ee1bb8ab3668d97fed3161b1bd30839008986e
Suman Khadka <dnatsu022@gmail.com>
2023-05-16 16:32:50 +0545

    url for logout

--- myFirstProject/accounts/urls.py
+++ myFirstProject/accounts/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('register', views.register, name='register'), # gives path to the register.html
     path('login', views.login, name= 'login'),
+    path('logout', views.logout, name= 'logout'),
 ]
