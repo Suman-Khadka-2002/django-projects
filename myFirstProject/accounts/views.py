6b8d038b4c29deadca6a942cfeb494cc87d24417
Suman Khadka <dnatsu022@gmail.com>
2023-05-16 16:02:27 +0545

    login page post request process

--- myFirstProject/accounts/views.py
+++ myFirstProject/accounts/views.py
@@ -4,6 +4,25 @@ from django.contrib.auth.models import User, auth
 
 # Create your views here.
 
+def login(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username = username, password = password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.error(request, 'Invalid credentials')
+            return redirect('login')
+
+    else:
+        return render(request, 'login.html')
+
+
+
 def register(request):
 
     if request.method == 'POST':  # post data
@@ -26,7 +45,8 @@ def register(request):
             else:
                 user = User.objects.create_user(username=username, password = password1, email=email, first_name=first_name, last_name=last_name)
                 user.save()
-                print('User created.') #prints in console after the user is created
+                print('User created.') # prints in console after the user is created
+                return redirect('login')
 
         else:
             messages.info(request, "password not matching.")
