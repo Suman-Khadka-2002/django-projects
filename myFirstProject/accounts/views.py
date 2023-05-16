f60c5bed6b0d6631d670c3af0163c1d60dc70398
Suman Khadka <dnatsu022@gmail.com>
2023-05-16 15:10:32 +0545

    username, password, email validation msg in console

--- myFirstProject/accounts/views.py
+++ myFirstProject/accounts/views.py
@@ -13,9 +13,20 @@ def register(request):
         password1 = request.POST['password1']
         password2 = request.POST['password2']
 
-        user = User.objects.create_user(username=username, password = password1, email=email, first_name=first_name, last_name=last_name)
-        user.save()
-        print('User created.') #prints in console after the user is created
+        if (password1 == password2):
+            if User.objects.filter(username = username).exists():
+                print('Username already taken.')
+            elif User.objects.filter(email = email).exists():
+                print("Email already taken.")
+            else:
+                user = User.objects.create_user(username=username, password = password1, email=email, first_name=first_name, last_name=last_name)
+                user.save()
+                print('User created.') #prints in console after the user is created
+                
+
+        else:
+            print("password not matching.")
+
         return redirect('/')  # redirects to homepage
     
     else:
