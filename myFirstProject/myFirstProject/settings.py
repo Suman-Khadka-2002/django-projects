6f653060c8a1592df7a65daffd3a2c1a45382b4d
Suman Khadka <dnatsu022@gmail.com>
2023-05-17 21:38:08 +0545

    updated

--- myFirstProject/myFirstProject/settings.py
+++ myFirstProject/myFirstProject/settings.py
@@ -77,8 +77,6 @@ WSGI_APPLICATION = 'myFirstProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'travello',
         'USER': 'root',
